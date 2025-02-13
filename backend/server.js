eef662a3718f9eac62649ed8d287aa332dcf33da
Nikita Gorenkov <kit.gorenkov@gmail.com>
2025-02-13 10:37:54 +0500

    Базы данных

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 // backend/server.js
+require('dotenv').config();
 const express = require('express'); // [Строка 1: Подключение express]
 const cors = require('cors');       // [Строка 2: Подключение cors]
 
 const app = express();
+const pool = require('./db');
 const port = process.env.PORT || 3000;
 
 // Middleware для обработки JSON и URL-кодированных данных
@@ -17,6 +19,17 @@ app.get('/', (req, res) => {      // [Строка 14]
   res.send('Сервер запущен!');     // [Строка 15]
 });
 
+app.get('/testdb', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT NOW()');
+      res.json({ status: 'success', time: result.rows[0].now });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ status: 'error', message: err.message });
+    }
+  });
+
+  
 // Запуск сервера
 app.listen(port, () => {          // [Строка 19]
   console.log(`Server listening on port ${port}`); // [Строка 20]
