f7e14b0545bfe0fb88628b58117f4681a00ec039
Nikita Gorenkov <kit.gorenkov@gmail.com>
2025-03-07 11:54:20 +0500

    12

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -29,21 +29,3 @@ const authMiddleware = (req, res, next) => {
 
 module.exports = authMiddleware;
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // "Bearer TOKEN"
-    
-    if (!token) {
-        return res.sendStatus(401); // Нет токена
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.sendStatus(403); // Токен недействителен
-        }
-        req.user = user;
-        next(); // Передаём управление следующему middleware или маршруту
-    });
-}
-
-module.exports = authenticateToken;
